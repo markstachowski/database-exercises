967cc8709917c07518c3a2872d7e599a405390b4
Mark Stachowski <markstachowski@me.com>
2018-12-04 12:26:25 -0600

    adding db updated exercises

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -1,38 +1,45 @@
 USE employees;
 
+# In your script, use DISTINCT to find the unique titles in the titles table. Your results should look like:
 SELECT DISTINCT title FROM titles;
 
-SELECT DISTINCT last_name
+# Find your query for employees whose last names start and end with 'E'. Update the query find just the unique last names that start and end with 'E' using GROUP BY. The results should be:
+SELECT last_name
 from employees
 where last_name like "e%"
   and last_name like "%e"
 GROUP BY last_name;
 
-SELECT DISTINCT first_name, last_name
+# Update your previous query to now find unique combinations of first and last name where the last name starts and ends with 'E'. You should get 846 rows.
+SELECT first_name, last_name
 from employees
 where last_name like "e%"
   and last_name like "%e"
 GROUP BY first_name, last_name;
 
-SELECT DISTINCT last_name
+##### Find the unique last names with a 'q' but not 'qu'. Your results should be:
+SELECT last_name
 from employees
 where last_name like "%q%"
   and last_name not like "%qu%"
 GROUP BY last_name;
 
-SELECT DISTINCT last_name, count(last_name)
+# Add a COUNT() to your results and use ORDER BY to make it easier to find employees whose unusual name is shared with others.
+SELECT last_name, count(last_name)
 from employees
 where last_name like "%q%"
   and last_name not like "%qu%"
 GROUP BY last_name
 ORDER BY count(last_name);
 
-SELECT DISTINCT gender, count(*)
+# Update your query for 'Irena', 'Vidya', or 'Maya'. Use count(*) and GROUP BY to find the number of employees for each gender with those names. Your results should be:
+
+SELECT gender, count(*)
 from employees
 where first_name IN("Irena","Vidya","Maya")
 GROUP BY gender;
 
-SELECT DISTINCT gender, count(*)
+SELECT gender, count(*)
 from employees
 where first_name IN("Irena","Vidya","Maya")
 GROUP BY gender
